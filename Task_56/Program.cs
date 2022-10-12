c27d8aed9452d2b248972cec9fd623e35ebf7f14
Sasha <sasha@example.com>
2022-10-12 14:02:10 +0300

    номер строки с минимальной суммой элементов

---
+++ Task_56/Program.cs
@@ -0,0 +1,51 @@
+﻿int[,] GenerateArray(int height, int weight)
+{
+    int[,] generatedArray = new int[height,weight];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < weight; j++)
+        {
+            generatedArray[i,j] = new Random().Next(0,10);
+        }
+    }
+    return generatedArray;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MinSummElevent (int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow +=  array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка наименьшая ");
+}
+
+int[,] Twoarray = GenerateArray(4,4);
+PrintArray(Twoarray);
+Console.WriteLine();
+MinSummElevent(Twoarray);
