5914ba2f0ce3f137a8247d6340a6dfbf434a14aa
Sasha <sasha@example.com>
2022-10-14 10:06:59 +0300

    обратный числовой ряд

---
+++ task_64/Program.cs
@@ -0,0 +1,17 @@
+﻿
+void printRangeNumber(int N)
+{
+    if(N == 1)
+    {
+        return;
+    }
+    Console.Write($"{N}, ");
+    printRangeNumber(N -1);
+}
+
+
+Console.WriteLine("Введите N");
+int usernumber =  Convert.ToInt32(Console.ReadLine());
+printRangeNumber(usernumber);
+ Console.Write(1);
+ Console.WriteLine();
