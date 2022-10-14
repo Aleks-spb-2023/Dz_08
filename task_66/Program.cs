aadf3c42210a5eb212d53e2a9083d0c899f16175
Sasha <sasha@example.com>
2022-10-14 12:00:34 +0300

    сумма элементов чисел от М до Н

---
+++ task_66/Program.cs
@@ -0,0 +1,13 @@
+﻿int GetSummRange(int M, int N)
+{
+    if(M == N + 1)
+    {
+        return 0;
+    }
+    return M + GetSummRange(M + 1,N);
+}
+Console.WriteLine("Введите начальное число ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное число число ");
+int end = Convert.ToInt32(Console.ReadLine());Console.WriteLine("Введите начальное число ");
+Console.WriteLine($"Сумма элементов от {start} до {end} = {GetSummRange(start,end)}");
